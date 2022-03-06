dbbff4ec97c3e01a2f2f5e7525a1e2a02aeeb9b0
horni <christopher.hornberger@gmail.com>
2022-03-06 20:01:09 +0100

    feat(vagrant): initial inventory plugin

--- vagrant.py
+++ vagrant.py
@@ -1,105 +1,124 @@
 #!/usr/bin/env python
 
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
 import os
+import subprocess
 import re
 
-from subprocess import Popen, PIPE
+from ansible.module_utils._text import to_text
+from ansible.plugins.inventory import BaseInventoryPlugin
 
-from ansible import constants as C
-from ansible.errors import AnsibleParserError
-from ansible.module_utils.common.text.converters import to_native, to_text
-from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
-from ansible.module_utils.common.process import get_bin_path
+class InventoryModule(BaseInventoryPlugin):
 
-class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
-    ''' Host inventory parser for ansible using local vagrant. '''
-
-    NAME = 'horni.collection.vagrant'
-#    find_box_user =  
-#    find_box_box = 
+    NAME = 'vagrant'
+    VAGRANT = "vagrant"
 
     def __init__(self):
+
+        self.virtualbox_path = None
         super(InventoryModule, self).__init__()
 
-    def _populate(self, hosts):
-        # Use constructed if applicable
-        strict = self.get_option('strict')
+    def _query_vagrant_data(self, vagrantline):
 
-        for host in hosts:
-            hostname = host['name']
-            self.inventory.add_host(hostname)
-            for var, value in host.items():
-                self.inventory.set_variable(hostname, var, value)
+        vagrantfile = vagrantline.split()[-2].strip() + '/Vagrantfile'
+        file = open(vagrantfile, "r")
+        for line in file:
+            os = re.search(r"config.vm.box.*\"(.*)\/(.*)\"", line)
+            if os:    
+                os_outer = str(os.group(1))
+                os_inner = str(os.group(2))
+                return(os_outer, os_inner)
 
-            # Composed variables
-            self._set_composite_vars(self.get_option('compose'), host, hostname, strict=strict)
+    def _query_boxname(self, vagrantline):
 
-            # Complex groups based on jinja2 conditionals, hosts that meet the conditional are added to group
-            self._add_host_to_composed_groups(self.get_option('groups'), host, hostname, strict=strict)
+        dirname = vagrantline.split()[-2].strip()
+        boxname = dirname.split('/')[-1]
+        return(boxname)
 
-            # Create groups based on variable values and add the corresponding hosts to it
-            self._add_host_to_keyed_groups(self.get_option('keyed_groups'), host, hostname, strict=strict)
+    def _populate_from_source(self, source_data, using_current_cache=False):
 
+        if using_current_cache:
+            self._populate_from_cache(source_data)
+            return source_data
+
+        cacheable_results = {'_meta': {'hostvars': {}}}
+
+        hostvars = {}
+        prevkey = pref_k = ''
+        current_host = None
+
+        netinfo = self.get_option('network_info_path')
+
+        for line in source_data:
+            line = to_text(line)
+            #if ':' not in line:
+            #    continue
+            try:
+                k, v = line.split('')
+            except Exception:
+                continue
+
+            if k.strip() == '':
+                continue
+
+            v = v.strip()
+
+            if "default" not in v:
+                continue
+
+            current_host = _query_boxname(v)
+ 
+            if current_host not in hostvars:
+                _query_vagrant_data(v)
+                hostvars[current_host] = {}
+                self.inventory.add_host(current_host)
+
+        
     def verify_file(self, path):
-
+        ''' return true/false if this is possibly a valid file for this plugin to consume '''
         valid = False
         if super(InventoryModule, self).verify_file(path):
-            file_name, ext = os.path.splitext(path)
-
-            if not ext or ext in C.YAML_FILENAME_EXTENSIONS:
+            # base class verifies that file exists and is readable by current user
+            if path.endswith(('vagrant.yaml', 'vagrant.yml')):
                 valid = True
-
         return valid
 
-    def list_all_boxes(self):
-        boxes = {}
+    def parse(self, inventory, loader, path, cache=True):
 
         try:
-            get_bin_path('vagrant')
+            self.vagrant_path = get_bin_path(self.VAGRANT)
         except ValueError as e:
-            raise AnsibleParserError('vagrant inventory plugin requires the vagrant cli tool to work {0}'.format(to_native(e)))
+            raise AnsibleParserError(e)
 
-        output = to_text(subprocess.check_output(["vagrant", "global-status", "--prune"]), errors='surrogate_or_strict').split('\n')
-    
-        for line in output:
-            dirname = re.search(r"(running.+?)(.[^\/].*)", line)
-            if dirname:
-                vagrantfile = str(dirname.group(2)).strip() + '/Vagrantfile'
-                file = open(vagrantfile, "r")
-                for line in file:
-                    linematch = re.search(r"config.vm.box.*\"(.*)\/(.*)\"", line)
-                    if linematch:    
-                        vagrant_user = str(linematch.group(1))
-                        vagrant_box = str(linematch.group(2))
-        boxes[matchfilename] = vagrant_user + '/' + vagrant_box
-        return boxes
-    
-    def get_ssh_config(self):
-        """
-        returns the ssh config for a box
-        """
-        return dict((k, get_a_ssh_config(k)) for k in boxes)
+        super(InventoryModule, self).parse(inventory, loader, path)
 
-    # get the ssh config for a single box
-    def get_a_ssh_config(self, box_name):
-        """Gives back a map of all the machine's ssh configurations"""
-    
-        output = to_text(subprocess.check_output(["vagrant", "ssh-config", box_name]))
-        config = SSHConfig()
-        config.parse(StringIO(output))
-        host_config = config.lookup("default")
-    
-        # man 5 ssh_config:
-        # > It is possible to have multiple identity files ...
-        # > all these identities will be tried in sequence.
-    
-        for identity in host_config['identityfile']:
-            if os.path.isfile(identity):
-                host_config['identityfile'] = identity
-    
-        return dict((v, host_config[k]) for k, v in _ssh_to_ansible)
-    
+        cache_key = self.get_cache_key(path)
+
+        config_data = self._read_config_data(path)
+
+        self._consume_options(config_data)
+
+        source_data = None
+        if cache:
+            try:
+                source_data = self._cache[cache_key]
+            except KeyError:
+                update_cache = True
+
+        if not source_data:
+            running = self.get_option('running_only')
+
+            cmd = [self._vagrant_path, 'global-status', '--prune']
+            
+            try:
+                p = Popen(cmd, stdout=Pipe)
+            except Exception as e:
+                raise AnsibleParserError(to_native(e))
+
+            source_data = p.stdout.read().splitlines()
+
+        using_current_cache = cache and not update_cache
+        cacheable_results = self._populate_from_source(source_data, using_current_cache)
+
+        if update_cache:
+            self._cache[cache_key] = cacheable_results
         
